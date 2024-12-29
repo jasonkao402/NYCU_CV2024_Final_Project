2ed46656cd9eba96115d187e36e6a01ec931b309
Darth-Phoenix <jeremy926.cs13@nycu.edu.tw>
2024-12-29 17:40:08 +0800

    minor change

--- MultiCamTriang.py
+++ MultiCamTriang.py
@@ -2,19 +2,18 @@ import numpy as np
 import cv2
 
 class MultiCamTriang(object):
-    """docstring for MultiCamTriang"""
     def __init__(self, poses, eye, Ks):
         super(MultiCamTriang, self).__init__()
-        self.poses = poses                   # shape:(num_cam, c2w(3, 3)) transform matrix from ccs to wcs
-        self.eye = eye                       # shape:(num_cam, 1, xyz(3)) camera position in wcs
-        self.Ks = Ks                         # shape:(num_cam, K(3,3)) intrinsic matrix
-        self.f = (Ks[:,0,0] + Ks[:,1,1]) / 2 # shape:(num_cam) focal length
-        self.p = Ks[:,0:2,2]                 # shape:(num_cam, xy(2)) principal point
+        self.poses = poses
+        self.eye = eye
+        self.Ks = Ks
+        self.f = (Ks[:,0,0] + Ks[:,1,1]) / 2
+        self.p = Ks[:,0:2,2]
         # self.projection_mat = projection_mat
 
     def setTrack2Ds(self, newtrack2Ds):     # must set, then run calculate3D
         #logging.debug('setting track2Ds:{newtrack2Ds}')
-        self.track2Ds = newtrack2Ds          # shape:(num_cam, num_frame, xy(2)) 2D track from TrackNetV2
+        self.track2Ds = newtrack2Ds
 
     def setProjectionMats(self, ProjectionMats):
         self.projection_mat = ProjectionMats
@@ -40,7 +39,3 @@ class MultiCamTriang(object):
         for i in track_3Ds:
             total_track3Ds += i
         return total_track3Ds/len(track_3Ds)
-        # self.track3D_homo = cv2.triangulatePoints(self.projection_mat[0],self.projection_mat[1],self.track2Ds[0][0],self.track2Ds[1][0]) # shape:(4,num_frame), num_frame=1
-        # self.track3D = self.track3D_homo[:3] / self.track3D_homo[3] # shape:(3,num_frame), num_frame=1
-        # self.track3D = np.stack(self.track3D, axis=1) # shape:(num_frame,3), num_frame=1
-        # return self.track3D
